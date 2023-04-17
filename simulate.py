2c1bd5454b682e2f4994868c3ca65eafbb40a3ba
sunnysoni <sunnysoni97@gmail.com>
2023-04-17 22:05:47 +0200

    centralised fedavg testing working!

--- simulate.py
+++ simulate.py
@@ -1,13 +1,13 @@
 import flwr as fl
 from flwr.server import strategy
 from client import FlowerClient
-from models import init_model, get_parameters
 import torch
-from typing import Dict, List
 import numpy as np
 import argparse
 from data_loader_scripts.download import download_dataset
 from data_loader_scripts.partition import do_fl_partitioning
+from strategy import common_functions, fed_avg_fn
+from torch.utils.data import DataLoader
 
 parser = argparse.ArgumentParser(description="FedAvg Simulation using Flower")
 
@@ -51,7 +51,11 @@ if __name__ == "__main__":
     else:
         raise ValueError(f"{DATASET_NAME} has not been implemented yet!")
 
-    train_data_path, test_loader = download_dataset(DATA_DIR, DATASET_NAME)
+    train_data_path, test_set = download_dataset(DATA_DIR, DATASET_NAME)
+    kwargs_test_loader = {"num_workers": CLIENT_CPUS,
+                          "pin_memory": True, "drop_last": False}
+    test_loader = DataLoader(
+        test_set, batch_size=BATCH_SIZE, **kwargs_test_loader)
 
     fed_dir = do_fl_partitioning(
         train_data_path, NUM_CLIENTS, PARTITION_ALPHA, NUM_CLASSES, SEED, PARTITION_VAL_RATIO)
@@ -59,61 +63,24 @@ if __name__ == "__main__":
     def client_fn(cid) -> FlowerClient:
         return FlowerClient(cid, MODEL_NAME, NUM_CLASSES, DATASET_NAME, fed_dir, BATCH_SIZE, CLIENT_CPUS, DEVICE)
 
-    def fit_config(server_round: int) -> Dict[str, float]:
-        config = {
-            'lr': 1e-3,
-            'epochs': 1,
-            'momentum': 0.9,
-        }
-        return config
-
     client_resources = {"num_cpus": CLIENT_CPUS}
     if (DEVICE.type == "cuda"):
         client_resources["num_gpus"] = CLIENT_GPUS
 
-    def initialise_parameters() -> List[np.ndarray]:
-        new_model = init_model(MODEL_NAME, NUM_CLASSES)
-        parameters = get_parameters(new_model)
-        return fl.common.ndarrays_to_parameters(parameters)
-
-    def fit_metrics_aggregation_fn(fit_metrics) -> Dict[str, float]:
-
-        total_clients = 0
-        total_train_loss = 0
-        total_train_acc = 0
-
-        for metrics in fit_metrics:
-            total_clients += 1
-            total_train_loss += metrics[1]['train_loss']
-            total_train_acc += metrics[1]['train_acc']
-
-        avg_train_loss = total_train_loss/total_clients
-        avg_train_acc = total_train_acc/total_clients
-
-        return {'avg_train_loss': avg_train_loss, 'avg_train_acc': avg_train_acc}
-
-    def evaluate_metrics_aggregation_fn(eval_metrics) -> Dict[str, float]:
-
-        total_clients = 0
-        total_eval_acc = 0
-
-        for metrics in eval_metrics:
-            total_clients += 1
-            total_eval_acc += metrics[1]['accuracy']
-
-        avg_eval_acc = total_eval_acc / total_clients
-
-        return {'avg_eval_acc': avg_eval_acc}
-
+    # FedAvg
     fl.simulation.start_simulation(
         client_fn=client_fn,
         num_clients=NUM_CLIENTS,
         config=fl.server.ServerConfig(num_rounds=NUM_ROUNDS),
         strategy=strategy.FedAvg(
-            on_fit_config_fn=fit_config,
-            initial_parameters=initialise_parameters(),
-            fit_metrics_aggregation_fn=fit_metrics_aggregation_fn,
-            evaluate_metrics_aggregation_fn=evaluate_metrics_aggregation_fn,
+            on_fit_config_fn=fed_avg_fn.get_fit_config_fn(),
+            on_evaluate_config_fn=fed_avg_fn.get_eval_config_fn(),
+            initial_parameters=common_functions.initialise_parameters(
+                MODEL_NAME, NUM_CLASSES),
+            fit_metrics_aggregation_fn=common_functions.fit_metrics_aggregation_fn,
+            evaluate_metrics_aggregation_fn=common_functions.evaluate_metrics_aggregation_fn,
+            evaluate_fn=fed_avg_fn.get_evaluate_fn(
+                MODEL_NAME, NUM_CLASSES, test_loader, DEVICE)
         ),
         client_resources=client_resources,
     )
---
+++ strategy.py
@@ -0,0 +1,81 @@
+from flwr.common import (
+    Parameters,
+    ndarrays_to_parameters
+)
+from models import init_model, get_parameters
+from typing import Dict, Tuple
+from torch.utils.data import DataLoader
+import torch
+from common import test_model
+
+
+class common_functions:
+    @staticmethod
+    def initialise_parameters(model_name: str, num_classes: int) -> Parameters:
+        new_model = init_model(model_name, num_classes)
+        parameters = get_parameters(new_model)
+        return ndarrays_to_parameters(parameters)
+
+    @staticmethod
+    def fit_metrics_aggregation_fn(fit_metrics) -> Dict[str, float]:
+
+        total_clients = 0
+        total_train_loss = 0
+        total_train_acc = 0
+
+        for metrics in fit_metrics:
+            total_clients += 1
+            total_train_loss += metrics[1]['train_loss']
+            total_train_acc += metrics[1]['train_acc']
+
+        avg_train_loss = total_train_loss/total_clients
+        avg_train_acc = total_train_acc/total_clients
+
+        return {'avg_train_loss': avg_train_loss, 'avg_train_acc': avg_train_acc}
+
+    @staticmethod
+    def evaluate_metrics_aggregation_fn(eval_metrics) -> Dict[str, float]:
+
+        total_clients = 0
+        total_eval_acc = 0
+
+        for metrics in eval_metrics:
+            total_clients += 1
+            total_eval_acc += metrics[1]['accuracy']
+
+        avg_eval_acc = total_eval_acc / total_clients
+
+        return {'avg_eval_acc': avg_eval_acc}
+
+
+class fed_avg_fn:
+    @staticmethod
+    def get_fit_config_fn():
+        def on_fit_config_fn(server_round: int) -> Dict[str, float]:
+            config = {
+                'lr': 1e-3,
+                'epochs': 1,
+                'momentum': 0.9,
+                'round': server_round
+            }
+            return config
+        return on_fit_config_fn
+
+    @staticmethod
+    def get_eval_config_fn():
+        def on_eval_config_fn(server_round: int) -> Dict[str, float]:
+            config = {
+                'round': server_round
+            }
+            return config
+        return on_eval_config_fn
+
+    @staticmethod
+    def get_evaluate_fn(model_name: str, n_classes: int, test_loader: DataLoader, device: torch.device):
+        def evaluate_fn(server_round: int, model_params: Parameters, unk_dict: Dict) -> Tuple[float, Dict[str, float]]:
+            test_res = test_model(model_name, n_classes,
+                                  model_params, test_loader, device)
+            test_loss = test_res['test_loss']
+            test_acc = test_res['test_acc']
+            return test_loss, {'server_test_acc': test_acc}
+        return evaluate_fn
