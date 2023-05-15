7579db29d2a9c770111e7005084555662f5ce551
sunnysoni <sunnysoni97@gmail.com>
2023-05-16 01:41:45 +0200

    cifar resnet is tested

--- models/cifar_resnet.py
+++ models/cifar_resnet.py
@@ -32,12 +32,6 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
 import torch.nn as nn
-try:
-    from torch.hub import load_state_dict_from_url
-except ImportError:
-    from torch.utils.model_zoo import load_url as load_state_dict_from_url
-
-from typing import Dict, Any, List
 
 
 def conv3x3(in_planes, out_planes, stride=1):
@@ -77,6 +71,67 @@ class BasicBlock(nn.Module):
             identity = self.downsample(x)
 
         out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(
+        self,
+        in_planes,
+        out_planes,
+        stride=1,
+        downsample=None,
+    ):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels=in_planes, out_channels=out_planes, kernel_size=1, bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(out_planes)
+
+        self.conv2 = nn.Conv2d(
+            in_channels=out_planes,
+            out_channels=out_planes,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False,
+        )
+        self.bn2 = nn.BatchNorm2d(out_planes)
+
+        self.conv3 = nn.Conv2d(
+            in_channels=out_planes,
+            out_channels=out_planes * 4,
+            kernel_size=1,
+            bias=False,
+        )
+        self.bn3 = nn.BatchNorm2d(out_planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
         out = self.relu(out)
 
         return out
@@ -138,7 +193,7 @@ class CifarResNet(nn.Module):
         return x
 
 
-def cifar_resnet8(dataset_name: str) -> CifarResNet:
+def cifar_resnet(dataset_name: str = "cifar10", variant: int = 8) -> CifarResNet:
     if(dataset_name == "cifar10"):
         num_classes = 10
     elif(dataset_name == "cifar100"):
@@ -146,6 +201,15 @@ def cifar_resnet8(dataset_name: str) -> CifarResNet:
     else:
         raise ValueError("Incorrect dataset name for cifar resnet!")
 
-    layers = [1]*3
-    model = CifarResNet(BasicBlock, layers, num_classes)
+    if(variant % 6 != 2):
+        raise ValueError("Incorrect variant number for resnet layers!")
+
+    layers = [(variant-2)//6]*3
+
+    if(variant > 44):
+        block_fn = Bottleneck
+    else:
+        block_fn = BasicBlock
+
+    model = CifarResNet(block_fn, layers, num_classes)
     return model
