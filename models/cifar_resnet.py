0ae2297164f5886c1e2fd4144102331c4cd49aaf
sunnysoni <sunnysoni97@gmail.com>
2023-06-06 12:58:52 +0200

    added alternate forward implementation for avgpool output

--- models/cifar_resnet.py
+++ models/cifar_resnet.py
@@ -191,6 +191,21 @@ class CifarResNet(nn.Module):
         x = self.fc(x)
 
         return x
+    
+    def forward_avgpool(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+
+        return x
+
 
 
 def cifar_resnet(dataset_name: str = "cifar10", variant: int = 8) -> CifarResNet:
