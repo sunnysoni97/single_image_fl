e49a759a3ed0a97296dd6042e894f489b2d7079e
sunnysoni <sunnysoni97@gmail.com>
2023-06-10 01:03:41 +0200

    implemented visualiser for clusters

--- strategy/clustering.py
+++ strategy/clustering.py
@@ -7,12 +7,15 @@ from cuml import KMeans as KMeans_GPU
 from sklearn.cluster import KMeans as KMeans_CPU
 
 from torch.utils.data import DataLoader
-from typing import Union, Tuple
+from typing import Union, Tuple, List
 from models import CifarResNet
 from torchvision.models import ResNet
 
+from torchvision.utils import make_grid, save_image
+from io import BufferedWriter
 
 # Function to form K-Mean clusters using the embeddings of the neural network
+
 
 def cluster_embeddings(dataloader: DataLoader, model: Union[CifarResNet, ResNet], device: torch.device, n_clusters: int = 10, seed: int = 1) -> Tuple[pd.DataFrame, float]:
 
@@ -78,81 +81,112 @@ def cluster_embeddings(dataloader: DataLoader, model: Union[CifarResNet, ResNet]
 # supported heuristics : easy / hard / mixed
 # heuristic percentage : easy-hard
 
-def prune_clusters(raw_dataframe:pd.DataFrame, n_crops:int=2250, heuristic:str="mixed", heuristic_percentage:str="50-50") -> pd.DataFrame:
+def prune_clusters(raw_dataframe: pd.DataFrame, n_crops: int = 2250, heuristic: str = "mixed", heuristic_percentage: str = "50-50") -> pd.DataFrame:
     df = raw_dataframe.copy(True)
-    df.insert(len(df.columns),"selected","no")
+    df.insert(len(df.columns), "selected", "no")
     n_clusters = len(df['cluster'].value_counts().index)
     min_n_crops = int(n_crops/n_clusters * 50/100)
-    
-    if(heuristic=='easy'):
+
+    if (heuristic == 'easy'):
         percent_easy = 100
         percent_hard = 50
 
-    elif(heuristic=='hard'):
+    elif (heuristic == 'hard'):
         percent_easy = 0
         percent_hard = 100
 
-    elif(heuristic=='mixed'):
+    elif (heuristic == 'mixed'):
         percent_list = heuristic_percentage.split(sep='-')
         percent_list = [int(x) for x in percent_list]
-        
-        if(not (len(percent_list)==2 and type(percent_list[0])==type(1) and percent_list[0]+percent_list[1] == 100)):
-            raise ValueError(f'{heuristic_percentage} is not correct way to pass percentage!')
-        
+
+        if (not (len(percent_list) == 2 and type(percent_list[0]) == type(1) and percent_list[0]+percent_list[1] == 100)):
+            raise ValueError(
+                f'{heuristic_percentage} is not correct way to pass percentage!')
+
         percent_easy = percent_list[0]
         percent_hard = percent_list[1]
 
     else:
-        raise NotImplementedError(f'{heuristic} has not been implemented for our kmeans!')
+        raise NotImplementedError(
+            f'{heuristic} has not been implemented for our kmeans!')
 
-    def select_minimum(x:pd.DataFrame):
+    def select_minimum(x: pd.DataFrame):
         n_easy = int(percent_easy/100*min_n_crops)
         n_hard = int(percent_hard/100*min_n_crops)
-        
-        if(n_easy + n_hard != min_n_crops):
+
+        if (n_easy + n_hard != min_n_crops):
             n_easy += (min_n_crops-(n_easy+n_hard))
 
-        x = x.sort_values(by='cluster_dist',ascending=True)
+        x = x.sort_values(by='cluster_dist', ascending=True)
         x.iloc[0:n_easy, x.columns.get_loc('selected')] = 'yes'
 
-        x = x.sort_values(by='cluster_dist',ascending=False)
+        x = x.sort_values(by='cluster_dist', ascending=False)
         x.iloc[0:n_hard, x.columns.get_loc('selected')] = 'yes'
 
-        x = x.sort_values(by='img_no',ascending=True)
+        x = x.sort_values(by='img_no', ascending=True)
 
         return x
-    
+
     df = df.groupby(by='cluster', group_keys=False).apply(select_minimum)
 
     out_df = df[df['selected'] == 'yes']
-    
+
     df = df[df['selected'] == 'no']
 
-    def fill_rest(x:pd.DataFrame):
+    def fill_rest(x: pd.DataFrame):
         rem_crops = n_crops - int(min_n_crops*n_clusters)
         n_easy = int(percent_easy/100*rem_crops)
         n_hard = int(percent_hard/100*rem_crops)
-        
-        if(n_easy + n_hard != rem_crops):
+
+        if (n_easy + n_hard != rem_crops):
             n_easy += (rem_crops-(n_easy+n_hard))
 
-        x = x.sort_values(by='cluster_dist',ascending=True)
+        x = x.sort_values(by='cluster_dist', ascending=True)
         x.iloc[0:n_easy, x.columns.get_loc('selected')] = 'yes'
 
-        x = x.sort_values(by='cluster_dist',ascending=False)
+        x = x.sort_values(by='cluster_dist', ascending=False)
         x.iloc[0:n_hard, x.columns.get_loc('selected')] = 'yes'
 
-        x = x.sort_values(by='img_no',ascending=True)
+        x = x.sort_values(by='img_no', ascending=True)
 
         return x
 
     df = fill_rest(df)
     df = df[df['selected'] == 'yes']
 
-    out_df = pd.concat([out_df,df]).reset_index(drop=True)
+    out_df = pd.concat([out_df, df]).reset_index(drop=True)
     out_df.index = list(out_df.index)
     out_df.index.name = 'img_no'
 
-    out_df.drop('selected',axis=1,inplace=True)
+    out_df.drop('selected', axis=1, inplace=True)
 
     return out_df
+
+
+# Function to visualise the images in the clusters
+
+def visualise_clusters(cluster_df: pd.DataFrame, file: BufferedWriter, n_classes: int = 5, n_imgs: int = 8) -> None:
+    cluster_list = list(cluster_df.value_counts('cluster').index)
+    selected_clusters = np.random.choice(
+        cluster_list, size=n_classes, replace=False)
+
+    def get_images(df: pd.DataFrame, cluster_name: int, n_images: int = 2) -> List[np.ndarray]:
+        imgs = df.groupby(by='cluster').get_group(cluster_name).sort_values(
+            by='cluster_dist').loc[:, ["img", "cluster_dist"]].reset_index()
+        easy_imgs = imgs.loc[0:n_images-1, "img"].tolist()
+        hard_imgs = imgs.loc[len(imgs)-n_images:len(imgs)-1, "img"].tolist()
+        return easy_imgs+hard_imgs
+
+    all_imgs = []
+    for cluster in selected_clusters:
+        cluster_imgs = get_images(cluster_df, cluster, n_imgs)
+        all_imgs += cluster_imgs
+
+    all_imgs = [torch.tensor(x) for x in all_imgs]
+    all_imgs = torch.cat(all_imgs, dim=0)
+
+    grid_imgs = make_grid(tensor=all_imgs, nrow=n_imgs,
+                          pad_value=0.2, normalize=True)
+
+    save_image(grid_imgs, file, format='png')
+    return
