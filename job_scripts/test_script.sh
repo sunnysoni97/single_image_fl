69801cc7b3138a59d3f9a7f92b4bd14fa414cafd
Sunny Soni <sunnysoni97@gmail.com>
2023-06-12 21:19:21 +0200

    changed script with new crops in account

--- job_scripts/test_script.sh
+++ job_scripts/test_script.sh
@@ -6,8 +6,12 @@
 #SBATCH --cpus-per-task 1
 #SBATCH --job-name=sunny_test_job
 
-module purge
-module load 2022
-module load Anaconda3/2022.05
+# module purge
+# module load 2022
+# module load Anaconda3/2022.05
 
-srun python test_file.py
+# srun python test_file.py
+
+DATA_DIR=$TMPDIR'/'`date +"%s_%N"`
+
+echo $DATA_DIR
