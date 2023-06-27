57e86e1d67707dbb0ed502ea31e9fb14e002141a
Sunny Soni <sunnysoni97@gmail.com>
2023-06-27 14:55:36 +0200

    time to add logger

---
+++ job_scripts/experiments/clip_factor_search.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Queuing jobs for clipping factor experiment..."
+
+clip_factor='0.5 1.0 1.5 2.0'
+
+for cf in $clip_factor
+do
+    sbatch job_scrips/jobfile_feddf.job -c $cf
+done
