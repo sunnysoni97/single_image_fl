ad798b5333edf7db90cac1a5d5b4e471e187573f
Sunny Soni <sunnysoni97@gmail.com>
2023-08-15 22:30:52 +0200

    new results : kmeans balancing factor

--- job_scripts/experiments/param_search.sh
+++ job_scripts/experiments/param_search.sh
@@ -5,16 +5,16 @@ echo "Queuing jobs for your hyper-parameter tuning experiments..."
 seeds='42 84'
 # server_lr='0.1 0.05 0.01 0.005 0.001'
 # client_lr='0.01'
-cluster_nums='5 10 50 100 1000'
-# balancing_factor='0.01 0.1 0.5 1.0'
+# cluster_nums='5 10 50 100 1000'
+balancing_factor='0.0 0.05 0.1 0.5 1.0'
 # entropy_thresholds='0.01 0.1 0.25 0.5 0.75' 
 
 
-for cluster_num in $cluster_nums
+for bf in $balancing_factor
 do
     for seed in $seeds
     do
-        sbatch job_scripts/jobfile_feddf.job -s $seed -k $cluster_num
+        sbatch job_scripts/jobfile_feddf.job -s $seed -b $bf
     done
 done
     
