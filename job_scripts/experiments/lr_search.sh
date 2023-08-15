223c10c6832c1585d6401b119158b3d702568466
Sunny Soni <sunnysoni97@gmail.com>
2023-08-15 10:46:21 +0200

    New results : global lr optimisation

--- job_scripts/experiments/lr_search.sh
+++ job_scripts/experiments/lr_search.sh
@@ -2,14 +2,19 @@
 
 echo "Queuing jobs for your hyper-parameter tuning experiments..."
 
-server_lr='0.005'
-client_lr='0.1 0.05 0.01 0.005 0.001'
+seeds='42 84'
+server_lr='0.1 0.05 0.01 0.005 0.001'
+client_lr='0.01'
+
 
 for slr in $server_lr
 do
     for llr in $client_lr
     do
-        sbatch job_scripts/jobfile_feddf.job -l $llr -g $slr
+        for seed in $seeds
+        do
+            sbatch job_scripts/jobfile_feddf.job -l $llr -g $slr -s $seed
+        done
     done
 done
     
