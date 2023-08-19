9a489fa006932ce4f26d23bfed90b93e457aa418
Sunny Soni <sunnysoni97@gmail.com>
2023-08-20 01:50:29 +0200

    New results : entropy ablations number

--- job_scripts/experiments/param_search.sh
+++ job_scripts/experiments/param_search.sh
@@ -6,16 +6,16 @@ seeds='42 84'
 # server_lr='0.1 0.05 0.01 0.005 0.001'
 # client_lr='0.01'
 # cluster_nums='5 10 50 100 1000'
-balancing_factor='0.0 0.05 0.1 0.5 1.0'
-# entropy_thresholds='0.01 0.1 0.25 0.5 0.75' 
+# balancing_factor='0.0 0.05 0.1 0.5 1.0'
+entropy_thresholds='0.1 0.5 0.9' 
 # fedprox_factors='0.0 0.5 1.0 1.5 2.0'
 
 
-for param in $balancing_factor
+for param in $entropy_thresholds
 do
     for seed in $seeds
     do
-        sbatch job_scripts/jobfile_feddf.job -s $seed -b $param
+        sbatch job_scripts/jobfile_feddf.job -s $seed -t $param
     done
 done
     
