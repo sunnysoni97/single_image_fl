5c855fd432cd004babd16a00b93206d66db0e1e9
Sunny Soni <sunnysoni97@gmail.com>
2023-08-19 19:07:43 +0200

    new results : selection ablation - kmeans balancing

--- job_scripts/experiments/param_search.sh
+++ job_scripts/experiments/param_search.sh
@@ -5,17 +5,17 @@ echo "Queuing jobs for your hyper-parameter tuning experiments..."
 seeds='42 84'
 # server_lr='0.1 0.05 0.01 0.005 0.001'
 # client_lr='0.01'
-cluster_nums='5 10 50 100 1000'
-# balancing_factor='0.0 0.05 0.1 0.5 1.0'
+# cluster_nums='5 10 50 100 1000'
+balancing_factor='0.0 0.05 0.1 0.5 1.0'
 # entropy_thresholds='0.01 0.1 0.25 0.5 0.75' 
 # fedprox_factors='0.0 0.5 1.0 1.5 2.0'
 
 
-for param in $cluster_nums
+for param in $balancing_factor
 do
     for seed in $seeds
     do
-        sbatch job_scripts/jobfile_feddf.job -s $seed -k $param
+        sbatch job_scripts/jobfile_feddf.job -s $seed -b $param
     done
 done
     
